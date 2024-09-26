14d02c2e03ff23919fdffe6a4dadc6bda4f7e744
Flacko <lesliejoe2k2@gmail.com>
2024-09-26 18:12:58 +0000

    feat:  navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,15 @@
 export default function NavBar() {
   return (
-    <nav className="flex justify-between items-center">
+    <nav className="flex justify-between items-center w-[90%] my-0 mx-auto py-4">
       <div>Flacko.dev</div>
-      <div>Home</div>
+      <div>
+        <ul className="flex items-center gap-3">
+          <li>Home</li>
+          <li>About</li>
+          <li>Portfolio</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </nav>
   );
 }
