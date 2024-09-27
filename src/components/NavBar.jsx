cf835319184f2876dd737578caa7b399ce5fab21
Flacko <lesliejoe2k2@gmail.com>
2024-09-27 22:03:04 +0000

    feat mobile popup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Link } from "react-router-dom";
+
 export default function NavBar() {
+  const [active, setActive] = useState(false);
+
+  const modalPopup = () => {
+    setActive(!active);
+    console.log("fired function");
+  };
+
   return (
-    <nav className="flex justify-between items-center w-[90%] my-0 mx-auto py-4">
+    <nav className="flex justify-between items-center w-[90%] my-0 mx-auto py-4 border-b shadow-md">
       <div>Flacko.dev</div>
-      <div>
+      <div className="hidden md:block">
         <ul className="flex items-center gap-3">
           <li>Home</li>
           <li>About</li>
@@ -10,6 +21,7 @@ export default function NavBar() {
           <li>Contact</li>
         </ul>
       </div>
+      <RxHamburgerMenu className="md:hidden" onClick={modalPopup} />
     </nav>
   );
 }
