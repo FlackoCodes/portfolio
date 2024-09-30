39a6735db0776176eed170c78b670adcf30b716f
Flacko <lesliejoe2k2@gmail.com>
2024-09-30 20:58:46 +0000

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Home from "./Home";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <NavBar />
       <Home />
+      <Footer />
     </>
   );
 }
