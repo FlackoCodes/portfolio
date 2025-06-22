26786f6e1149e66601ca10cfd7391609873b21dc
Flacko <lesliejoe2k2@gmail.com>
2025-06-22 08:08:16 +0000

    feat | added links to navbar components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -22,11 +22,12 @@ export default function NavBar() {
           </div>
           <div className="hidden md:block">
             <ul className="flex items-center gap-3">
-              <li>Home</li>
-              <li>About</li>
-              <li>Portfolio</li>
-              <li>Contact</li>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#portfolio">Portfolio</a></li>
+              <li><a href="#contact">Contact</a></li>
             </ul>
+
           </div>
           <div className="md:hidden cursor-pointer" onClick={modalPopup}>
             {/* {active ? (
